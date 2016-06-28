2e7428cfbcd9e7bae650723a35d2e96abfbd38ac
Jacob Gillespie <jacobwgillespie@gmail.com>
2016-06-27 21:23:12 -0500

    Add better message queue handling and message forwarding

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import random from 'secure-random';
+import uuid from 'node-uuid';
 
 const logElement = document.getElementById('log');
 
@@ -9,18 +10,13 @@ skylink.init('8833f5bc-aced-4342-ae3b-d334c36f3910');
 
 skylink.joinRoom();
 
-skylink.on('incomingMessage', (res, peerID, peerInfo, isSelf) => {
-  if (isSelf) return;
+const originID = uuid.v4();
 
-  const message = JSON.parse(res.content);
-  if (!message.message) return;
-  console.log('got message', message);
-  const text = Buffer.from(message.message, 'hex').toString();
-  logElement.innerHTML = `${logElement.innerHTML}<p>${res.content.length} - ${text}</p>`;
-});
+const seen = new Set();
 
-const queueSlotCount = 100;
-const sendDelay = 0;
+const queueSlotCount = 10;
+const slotFillMax = 2;
+const createDelay = 200;
 const messageQueue = [];
 for (let i = 0; i < queueSlotCount; i++) {
   messageQueue[i] = [];
@@ -29,37 +25,83 @@ for (let i = 0; i < queueSlotCount; i++) {
 const messageMaxLength = 1024;
 const packetSize = 2048;
 
+let messagesPerSecond = 0;
+
 const padding = original => {
   if (original.length > messageMaxLength) throw new Error('message is too long');
   const necessaryPadding = packetSize - original.length;
   return random.randomBuffer(necessaryPadding);
 };
 
-const buildMessage = message => ({
+const buildMessage = (slot, message) => ({
+  id: uuid.v4(),
+  originID,
+  slot,
   message: Buffer.from(message).toString('hex'),
   padding: padding(message).toString('hex'),
 });
+
+const sendQueue = [];
 
 const messageLoop = window.messageLoop = (slot = 0) => {
   if (slot >= queueSlotCount) {
     return setTimeout(() => {
       messageLoop(0);
-    }, sendDelay);
+    }, createDelay);
   }
 
   // console.log('sending message to slot', slot);
 
-  const message = messageQueue[slot].pop();
-
-  skylink.sendP2PMessage(JSON.stringify(buildMessage(message || '')));
+  if (messageQueue[slot].length < slotFillMax) messageQueue[slot].push(buildMessage(slot, ''));
+  const message = messageQueue[slot].shift() || buildMessage(slot, '');
+  seen.add(message.id);
+  sendQueue.push(message);
 
   return setTimeout(() => {
     messageLoop(slot + 1);
-  }, sendDelay);
+  }, createDelay);
 };
 
 window.sendMessage = (slot, message) => {
-  if (slot >= 0 && slot <= queueSlotCount) {
-    messageQueue[slot].push(message);
-  }
+  if (slot < 0 && slot >= queueSlotCount) return;
+  messageQueue[slot].unshift(buildMessage(slot, message));
 };
+
+skylink.on('incomingMessage', (res, peerID, peerInfo, isSelf) => {
+  if (isSelf) return;
+
+  const message = JSON.parse(res.content);
+
+  if (seen.has(message.id)) return;
+
+  seen.add(message.id);
+
+  sendQueue.push(message);
+
+  // console.log('got message', message.id, message.originID, message.message);
+
+  if (!message.message) return;
+  const text = Buffer.from(message.message, 'hex').toString();
+  logElement.innerHTML = `
+    <p>${Date.now()} - ${res.content.length} - ${text}</p>${logElement.innerHTML}
+  `.trim();
+});
+
+const mpsElement = document.getElementById('mps');
+const qsizeElement = document.getElementById('qsize');
+
+const sendLoop = window.sendLoop = () => {
+  const message = sendQueue.shift();
+  if (message) {
+    skylink.sendP2PMessage(JSON.stringify(message));
+    messagesPerSecond += 1;
+  }
+
+  setTimeout(sendLoop, 10);
+};
+
+setInterval(() => {
+  mpsElement.innerHTML = messagesPerSecond;
+  qsizeElement.innerHTML = sendQueue.length;
+  messagesPerSecond = 0;
+}, 1000);
